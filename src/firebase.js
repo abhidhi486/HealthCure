c704d1c649214478d6e9622b7bb04fe04e978e81
Abhishek Dhiman <abhidhi.486@gmail.com>
2026-07-01 17:37:29 +0530

    Firebase connected

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB9Knj2mOrMzzsauwe5GGfXe1FBTZu-P9k",
+  authDomain: "healthcure-e332a.firebaseapp.com",
+  projectId: "healthcure-e332a",
+  storageBucket: "healthcure-e332a.firebasestorage.app",
+  messagingSenderId: "222697995022",
+  appId: "1:222697995022:web:e09624d92ca4b2c7bc355f"
+};
+
+const app = initializeApp(firebaseConfig);
